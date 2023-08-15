f742c6b4923ead4e9733f0cce99e254a49553324
Mayon-Francis <mayonfrancis@gmail.com>
2023-08-15 13:21:10 +0530

    Init Express, Typescript, Logger, Sample route, Env

---
+++ Mailer-Backend/src/utils/env.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import logger from './logger';
+dotenv.config();
+
+type IEnv = {
+  PORT: number;
+};
+
+/**
+ * Values will be initialized from .env on server start
+ * by calling initEnv()
+ */
+export const ENV: IEnv = {
+  PORT: 5000,
+};
+
+export function initEnv() {
+  if (process.env.PORT) {
+    ENV.PORT = parseInt(process.env.PORT);
+  } else {
+    logger.info('No PORT specified in .env, using default 5000');
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Excel-Mailer-Service
+
+- First install dependencies: `yarn install`
+- Run Development server: `yarn dev`
+- Run Prod server: `yarn start`
